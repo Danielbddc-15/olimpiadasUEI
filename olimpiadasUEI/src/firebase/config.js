317b2f6a6c3ab48a4908fc6848b22094bdce8538
Bryan Daniel <danielito15deagosto@hotmail.com>
2025-08-21 11:11:41 -0500

    Actualización final para la implementación y configuracion de nueva base de datos

--- olimpiadasUEI/src/firebase/config.js
+++ olimpiadasUEI/src/firebase/config.js
@@ -2,23 +2,19 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDa6hEAkkYZJfRk1YsJbi6nK4rj5DkUZvo",
-  authDomain: "olimpiadasuei-683a1.firebaseapp.com",
-  projectId: "olimpiadasuei-683a1",
-  storageBucket: "olimpiadasuei-683a1.firebasestorage.app",
-  messagingSenderId: "394543410338",
-  appId: "1:394543410338:web:b06626c4a1cbd91fb53bfb",
-  measurementId: "G-G9Y30ECW16"
+  apiKey: "AIzaSyAkOKiV5U4qkGMd5pdT1hAwaLd8sDN9AMU",
+  authDomain: "olimpiadasuei-2025.firebaseapp.com",
+  projectId: "olimpiadasuei-2025",
+  storageBucket: "olimpiadasuei-2025.firebasestorage.app",
+  messagingSenderId: "676158035440",
+  appId: "1:676158035440:web:aa679ea5d275b3c0c17935"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
